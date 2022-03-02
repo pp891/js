2cb0d46690dbebe065e246cf539c0c4df6954456
pp891 <paolapavese@gmail.com>
2022-03-01 23:22:35 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
-let montoTotal = prompt("¿Cuál es el monto total gastado?");
-let comensales = prompt("¿Cuántas personas pagan la cuenta?");
-let porcentajePropina = prompt("¿Qué porcentaje desean dejar como propina?");
+let visitor = prompt("¿Cuál es tu nombre?");
 
-function calculoPropina() {
-    const propina = montoTotal * (porcentajePropina /100);
-    alert("La propina total es de: $" + propina);
+function greeting() {
+    alert("¡Hola, " + visitor + "!");
 }
 
-function propinaPorComensal() {
-    const propina = (montoTotal * (porcentajePropina / 100)) / comensales;
-    alert("Cada comensal dejará $" + propina + " de propina.");
+greeting();
+
+function Birthday(day, month) {
+    this.day = parseInt(prompt("¿Qué día cumplís años?"));
+    this.month = prompt("¿En qué mes cumplís años? Por favor, completar en letras");
+    this.birthday = "Registramos que tu cumpleaños es el: " + this.day + " de " + this.month;
 }
 
-function gastoTotalComensal() {
-    const total = (montoTotal / comensales) + ((montoTotal * (porcentajePropina / 100)) / comensales);
-    alert("Cada persona pagará $" + total + " en total.")
+const visitor1 = new Birthday ();
+
+function checkBirthday() {
+    alert(visitor1.birthday);
 }
 
-calculoPropina();
-propinaPorComensal();
-gastoTotalComensal();
+checkBirthday();
+
+function showSign() {
+    if ((visitor1.month == "enero" && visitor1.day <= 20) || (visitor1.month == "diciembre" && visitor1.day >= 21)) {
+        alert("Sos de Capricornio");
+        } else {
+            if ((visitor1.month == "febrero" && visitor1.day <= 19) || (visitor1.month == "enero" && visitor1.day >= 21)) {
+                alert("Sos de Acuario");
+            }   else {
+                if ((visitor1.month == "marzo" && visitor1.day <= 20) || (visitor1.month == "febrero" && visitor1.day >= 20)) {
+                    alert("Sos de Piscis");
+                } else {
+                    if ((visitor1.month == "abril" && visitor1.day <= 20) || (visitor1.month == "marzo" && visitor1.day >= 21)) {
+                        alert("Sos de Aries");
+                    } else {
+                        if ((visitor1.month == "mayo" && visitor1.day <= 20) || (visitor1.month == "abril" && visitor1.day >= 21)) {
+                            alert("Sos de Tauro");
+                        } else {
+                            if ((visitor1.month == "junio" && visitor1.day <= 21) || (visitor1.month == "mayo" && visitor1.day >= 21)) {
+                                alert("Sos de Géminis");
+                            } else {
+                                if ((visitor1.month == "julio" && visitor1.day <= 22) || (visitor1.month == "junio" && visitor1.day >= 22)) {
+                                    alert("Sos de Cáncer");
+                                } else {
+                                    if ((visitor1.month == "agosto" && visitor1.day <= 23) || (visitor1.month == "julio" && visitor1.day >= 23)) {
+                                        alert("Sos de Leo");
+                                    } else {
+                                        if ((visitor1.month == "septiembre" && visitor1.day <= 22) || (visitor1.month == "agosto" && visitor1.day >= 24)) {
+                                            alert("Sos de Virgo");
+                                        } else {
+                                            if ((visitor1.month == "octubre" && visitor1.day <= 22) || (visitor1.month == "septiembre" && visitor1.day >= 23)) {
+                                                alert("Sos de Libra");
+                                            } else {
+                                                if ((visitor1.month == "noviembre" && visitor1.day <= 21) || (visitor1.month == "octubre" && visitor1.day >= 23)) {
+                                                    alert("Sos de Escorpio");
+                                                } else {
+                                                    if ((visitor1.month == "diciembre" && visitor1.day <= 21) || (visitor1.month == "noviembre" && visitor1.day >= 23)) {
+                                                        alert("Sos de Sagitario")
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+}
+
+showSign();
+
+const sign = ["Aries", "Tauro", "Géminis", "Cáncer", "Leo", "Virgo", "Libra", "Escorpio", "Sagitario", "Capricornio", "Acuario", "Piscis"];
+
+let log = console.log(showSign())
